27b2c2a85156d72b7761547d75da2c3f5340e246
TLdgh <teng.li2@mail.mcgill.ca>
2023-10-06 08:57:00 -0400

    added disconnect to make sure

--- src/ScheduleDownload.R
+++ src/ScheduleDownload.R
@@ -7,3 +7,5 @@ tws <- twsConnect(port = 7496) #to connect with TWS
 source("/Users/tengli/R/TradingAlg/src/FuturesInfo.R")
 FutToBePrepared<-GetFutInfo(FUT=c("NQ"),interval=c("5F"))
 source("/Users/tengli/R/TradingAlg/src/PrepFutures.R")  #This src downloads the data and prepare for combination
+
+twsDisconnect(tws)#to disconnect
