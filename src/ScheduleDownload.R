7faa23dd39a078722b32a87a4b597a23d541e302
TLdgh <teng.li2@mail.mcgill.ca>
2024-04-21 16:02:16 -0400

    integrated ScheduleDownload with the new FuturesInfo.

--- src/ScheduleDownload.R
+++ src/ScheduleDownload.R
@@ -5,7 +5,6 @@ library(IBrokers)
 tws <- twsConnect(port = 7496) #to connect with TWS         
 
 source("/Users/tengli/R/TradingAlg/src/FuturesInfo.R")
-FutToBePrepared<-GetFutInfo(FUT=c("NQ"),interval=c("5F"))
-source("/Users/tengli/R/TradingAlg/src/PrepFutures.R")  #This src downloads the data and prepare for combination
+FutToBePrepared<-GetFutInfo(tws, FUT=c("NQ"),interval=c("5F"), RealData=TRUE)
 
 twsDisconnect(tws)#to disconnect
