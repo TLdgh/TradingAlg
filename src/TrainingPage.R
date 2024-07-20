fd9246d12e20c4298707ec596fbe80f204fd6c3d
TLdgh <teng.li2@mail.mcgill.ca>
2024-07-20 19:13:25 -0400

    Updated PAT.

--- src/TrainingPage.R
+++ src/TrainingPage.R
@@ -1,5 +1,5 @@
 Quantity <-c(2)
-T..Price<-c(9214)
+T..Price<-c()
 Code<-sapply(Quantity, function(x) ifelse(x>0, "O","C"))     
 data<-data.frame(Symbol="NQ",Quantity,T..Price,Leverage=2, Comm.Fee=0,Code)
 PnL(data)
@@ -10,6 +10,10 @@ PnL(data)
 ChartReplay(Data=list(SPY_daily=SPY_daily),Title="SPY_daily",PausePeriod = 4)
 
 #NQ:
-ChartReplay(Data=list(NQ4HContinuous=NQ4HContinuous, NQContinuous=NQContinuous, NQWContinuous=NQWContinuous), PausePeriod = 4)
+ChartReplay(Data=list(NQ30FContinuous=NQ30FContinuous, NQ4HContinuous=NQ4HContinuous, NQContinuous=NQContinuous), PausePeriod = 7)
+
+ChartReplay(Data=list(NQ30FContinuous=NQ30FContinuous, NQ4HContinuous=NQ4HContinuous, NQContinuous=NQContinuous), 
+            StartDate = "2020-02-18 02:00:00", PausePeriod = 7)
 
 
+292+422+106+1518
