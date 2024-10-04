3dc835f610dcadaf15d1be284cabd89c85503b92
TLdgh <teng.li2@mail.mcgill.ca>
2024-10-04 13:09:51 -0400

    minor update.

--- src/ScheduleDownload.R
+++ src/ScheduleDownload.R
@@ -6,5 +6,5 @@ tws <- twsConnect(port = 7496) #to connect with TWS
 
 source("/Users/tengli/R/TradingAlg/src/FuturesInfo.R")
 FutToBePrepared<-GetFutInfo(tws, FUT=c("NQ"),interval=c("5F"), RealData=TRUE)
-
+Sys.sleep(3)
 twsDisconnect(tws)#to disconnect
