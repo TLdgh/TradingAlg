d0d36282e5edc8f2739b65345340ad4e2ea6f60e
TLdgh <teng.li2@mail.mcgill.ca>
2024-11-01 22:07:59 -0400

    added Try Except for SectorScreener.

--- src/Screener.py
+++ src/Screener.py
@@ -5,41 +5,47 @@ from datetime import date
 import os, time, requests, logging, warnings
 
 def SectorScreener(root_directory):
-  foverview = Overview()
-  
-  Sectors=["Basic Materials","Communication Services","Consumer Cyclical","Consumer Defensive",
-           "Energy","Financial","Healthcare", "Industrials","Real Estate","Technology","Utilities"]
-  AdditionalTickers=["ACN","NXPI","TEL","LIN","LYB","SW","AMCR","ETN","TT","JCI","PNR","ALLE",
-                     "MDT","STE","CB","AON","ACGL","WTW","EG","LULU","NVR","GRMN","APTV"]
-  
-  df_list=[]
-  for s in Sectors:
-    filters_dict = {'Sector':s, 'Country': "USA", 'Price':'Over $5','Average Volume':'Over 100K'}
-    foverview.set_filter(filters_dict=filters_dict)
-    df = foverview.screener_view()
-    df = df.dropna(subset=["Market Cap"])
-    df_list.append(df)
-  
-  for t in AdditionalTickers:
-    stock=finvizfinance(t)
-    stock_fundament=stock.ticker_fundament()
-    stock_fundament['Ticker']=t    
-    stock_fundament["Market Cap"]=float(stock_fundament['Market Cap'].strip("B"))*1000000000
-    stock_fundament["P/E"]=float(stock_fundament['P/E'])
-    stock_fundament["Price"]=float(stock_fundament['Price'])
-    stock_fundament["Change"]=float(stock_fundament['Change'].strip("%"))/100
-    stock_fundament["Volume"]=int(stock_fundament['Volume'].replace(",",""))
-    stock_fundament=pd.DataFrame(stock_fundament, index=[0])
-    df = stock_fundament[['Ticker','Company','Sector','Industry','Country','Market Cap','P/E', 'Price','Change','Volume']]
-    df_list.append(df)
-    time.sleep(5)
-    
-  res=pd.concat(df_list, axis=0, ignore_index=True)
-  
-  current_date = date.today()
-  current_date = str(current_date).replace("-", "")
-  res.to_csv(root_directory+"/Data/OriginalStockData/US/SectorDistribution/distribution_"+current_date+".csv", index=False)
+  try:
+      foverview = Overview()
+      
+      Sectors=["Basic Materials","Communication Services","Consumer Cyclical","Consumer Defensive",
+              "Energy","Financial","Healthcare", "Industrials","Real Estate","Technology","Utilities"]
+      AdditionalTickers=["ACN","NXPI","TEL","LIN","LYB","SW","AMCR","ETN","TT","JCI","PNR","ALLE",
+                        "MDT","STE","CB","AON","ACGL","WTW","EG","LULU","NVR","GRMN","APTV"]
+      
+      df_list=[]
+      for s in Sectors:
+        filters_dict = {'Sector':s, 'Country': "USA", 'Price':'Over $5','Average Volume':'Over 100K'}
+        foverview.set_filter(filters_dict=filters_dict)
+        df = foverview.screener_view()
+        df = df.dropna(subset=["Market Cap"])
+        df_list.append(df)
+      
+      for t in AdditionalTickers:
+        stock=finvizfinance(t)
+        stock_fundament=stock.ticker_fundament()
+        stock_fundament['Ticker']=t    
+        stock_fundament["Market Cap"]=float(stock_fundament['Market Cap'].strip("B"))*1000000000
+        stock_fundament["P/E"]=float(stock_fundament['P/E'].replace("-","0"))
+        stock_fundament["Price"]=float(stock_fundament['Price'])
+        stock_fundament["Change"]=float(stock_fundament['Change'].strip("%"))/100
+        stock_fundament["Volume"]=int(stock_fundament['Volume'].replace(",",""))
+        stock_fundament=pd.DataFrame(stock_fundament, index=[0])
+        df = stock_fundament[['Ticker','Company','Sector','Industry','Country','Market Cap','P/E', 'Price','Change','Volume']]
+        df_list.append(df)
+        time.sleep(5)
+        
+      res=pd.concat(df_list, axis=0, ignore_index=True)
+      
+      current_date = date.today()
+      current_date = str(current_date).replace("-", "")
+      full_path=root_directory+"/Data/OriginalStockData/US/SectorDistribution/distribution_"+current_date+".csv"
+      res.to_csv(full_path, index=False)
 
+      logging.info(f"File downloaded successfully and saved to: {full_path}")
+    
+  except TypeError as e:
+      logging.error(f"An error occurred: {e}")
 
 def downloadVIX(root_directory):
   vix_url="https://cdn.cboe.com/api/global/us_indices/daily_prices/VIX_History.csv"
